2014183a0e1bb5b32b89645ee4109f55a79142ec
mohamed thabet <92416406+MAthabet@users.noreply.github.com>
2022-08-15 15:41:44 +0200

    first online lab
    goal is to get familiar with printf() and escape sequence in C language

---
+++ sessions/day1/lab1_newLineChar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+		printf("\n I'm mohamed alaa \
+		\n\n My birth date is 1 jan 2001 \
+		\n\nI will Graduate from Zagazig University in 2023\n");
+		
+		return 0;
+}
