403bb7c34acf01321ef523cd95a1c0d55394d8e0
mohamed thabet <92416406+MAthabet@users.noreply.github.com>
2022-08-17 07:33:37 +0200

    Delete sessions directory

--- sessions/Day5/lab1_pointers.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int x = 10;
-    printf("x beofre change = %d", x);
-    int *ptr = &x;
-    *ptr = 20;
-    printf("\nx after change = %d", x);
-}
--- sessions/Day6/def_lab1/std_int.h
+++
@@ -1,2 +0,0 @@
-typedef char int8_t;
-typedef unsigned char int16_t;
